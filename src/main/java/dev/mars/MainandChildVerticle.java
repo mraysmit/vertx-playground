9c0cca79e85cf6c7aa5ad1d6d17cbf6b8d635606
mraysmit <raysmith.subs@gmail.com>
2025-06-05 19:56:44 +0800

    refactor: enhance API Gateway initialization and error handling

--- src/main/java/dev/mars/MainandChildVerticle.java
+++ src/main/java/dev/mars/MainandChildVerticle.java
@@ -4,11 +4,11 @@ import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
 
-public class MainVerticle extends AbstractVerticle {
+public class MainandChildVerticle extends AbstractVerticle {
 
     public static void main(String[] args) {
         final Vertx vertx = Vertx.vertx();
-        vertx.deployVerticle(new MainVerticle());
+        vertx.deployVerticle(new MainandChildVerticle());
     }
 
     @Override
